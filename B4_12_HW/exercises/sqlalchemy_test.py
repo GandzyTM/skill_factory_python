4511e715abb051e70d2fc88657d65d54ebaefdac
GandzyTM <gandzy1984@gmail.com>
2020-06-16 21:03:31 +0500

    B4_7 begin

--- B4_12_HW/exercises/sqlalchemy_test.py
+++ B4_12_HW/exercises/sqlalchemy_test.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 DB_PATH = "sqlite:///../../data/b4_7.sqlite3"
 Base = declarative_base()
 
+
 class Album(Base):
     """Описывает структуру таблицы album для хранения записей музыкальной библиотеки"""
     __tablename__ = "album"
@@ -14,10 +15,11 @@ class Album(Base):
     genre = sa.Column(sa.TEXT)
     album = sa.Column(sa.TEXT)
 
+
 def parse_connection_string(connection_string):
     """Принимает на вход строку соединения connection_string и возвращает словарь с ее составными частями"""
 
-
+    return dict_parsed
 
 
 def main():
@@ -28,6 +30,10 @@ def main():
     # print(len(albums))
     for album in albums:
         print(f"Группа {album.artist} записала альбом {album.album} в {album.year} году.")
+
+    parse_connection_string("sqlite3:///b4_7.sqlite33")
+    parse_connection_string("postgresql+psycopg22://admin2:12342@localhost2:22/b4_72")
+    parse_connection_string("m2sql://a2dmin:21234/b24_7")
 
 if __name__ == "__main__":
     main()
