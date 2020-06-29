5fc4f50f8aedfc9961d99f27ab2b674f0d661d6f
gandzytm <gandzy1984@gmail.com>
2020-06-29 16:26:41 +0500

    B5.9 HW Class decorator

--- B5/HW/fibonachchi.py
+++ B5/HW/fibonachchi.py
@@ -1,3 +1,32 @@
+import time
+from tqdm import tqdm
+class Decorator_avg:
+    """B5.9 HW with * & **"""
+    def __init__(self, num_exec):
+        self.num_exec = num_exec
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    def __call__(self, function):
+        def wrapper(*args):
+            avg_time = 0
+            for _ in tqdm(range(self.num_exec)):
+                t0 = time.time()
+
+                function(*args)
+
+                t1 = time.time()
+                avg_time += (t1 - t0)
+            avg_time /= self.num_exec
+            return "Выполнение задания в среднем занимает %.3f секунд" % avg_time
+
+        return wrapper
+
+
 def fibonacci(n):
     if n < 3:
         return n
@@ -8,8 +37,6 @@ def fibonacci(n):
 def avg_decorator(num_exec):
     """B5.9 HW Декоратор с аргументом num_exec для основного декоратора"""
     def avg_time_exec(function):
-        import time
-        from tqdm import tqdm
         def wrapper(arg1, arg2):
             avg_time = 0
             for _ in tqdm(range(num_exec)):
@@ -27,7 +54,8 @@ def avg_decorator(num_exec):
     return avg_time_exec
 
 
-@avg_decorator(5) # передаем количество проходов функции декоратору
+# @avg_decorator(5) # передаем количество проходов функции декоратору (как функция)
+@Decorator_avg(5) # декоратор как класс
 def find_fib_nums(max_num_of_range, max_num_of_end):
     result = 0
     for i in range(max_num_of_range):
