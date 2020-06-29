8eac39975908299d6c23cbabcd60eb9a43832ef7
gandzytm <gandzy1984@gmail.com>
2020-06-29 11:43:41 +0500

    B5.9 HW

--- B5/exercises/fibonachchi.py
+++ B5/exercises/fibonachchi.py
@@ -5,29 +5,44 @@ def fibonacci(n):
         return (fibonacci(n - 1) + fibonacci(n - 2))
 
 
-def time_this(function):
-    import time
+def avg_decorator(num_exec):
+    """Декоратор с аргументом num_exec для основного декоратора"""
+    def avg_time_exec(function):
+        import time
+        from tqdm import tqdm
+        def wrapper(arg1, arg2):
+            avg_time = 0
+            for _ in tqdm(range(num_exec)):
+                t0 = time.time()
 
-    def wrapper():
-        start = time.time()
-        function()
-        end = time.time()
-        print("Время выполнения составило: {} скунд".format(end - start))
+                function(arg1, arg2)
 
-    return wrapper
+                t1 = time.time()
+                avg_time += (t1 - t0)
+            avg_time /= num_exec
+            return "Выполнение задания в среднем занимает %.3f секунд" % avg_time
+
+        return wrapper
+
+    return avg_time_exec
 
 
-@time_this
-def main():
+@avg_decorator(5) # передаем количество проходов функции декоратору
+def find_fib_nums(max_num_of_range, max_num_of_end):
     result = 0
-    for i in range(50):
+    for i in range(max_num_of_range):
         a = fibonacci(i)
         if a % 2 == 0:
             result += a
-        if a >= 10000000:
+        if a >= max_num_of_end:
             break
+    return result
 
-    print(result)
+
+def main():
+    print(find_fib_nums(50, 10000000))
+    # передаем 1 аргументом до какой цифры функция Фибоначчи будет проводить свои действия
+    # 2 аргументом передается лимит суммы получающихся чисел
 
 
 if __name__ == "__main__":
--- B5/exercises/timing.py
+++ B5/exercises/timing.py
@@ -1,18 +1,38 @@
-def main():
+# func_runtimes = {}
+# def profile(method):
+#     import time
+#     """ Profiling decorator. """
+#     def wrapper(*args, **kw):
+#         start_time = time.time()
+#
+#         result = method(*args, **kw)
+#
+#         elapsed_time = time.time() - start_time
+#         func_runtimes.setdefault(method.__name__, []).append(elapsed_time)
+#         return result
+#     return wrapper  # Decorated method (need to return this).
+#
+# def print_stats(method_name):
+#     if method_name not in func_runtimes:
+#         print("{!r} wasn't profiled, nothing to display.".format(method_name))
+#     else:
+#         runtimes = func_runtimes[method_name]
+#         total_runtime = sum(runtimes)
+#         average = total_runtime / len(runtimes)
+#         print('Stats for method: {!r}'.format(method_name))
+#         print('  run times: {}'.format(runtimes))
+#         print('  total run time: {}'.format(total_runtime))
+#         print('  average run time: {}'.format(average))
+
+
+def avg_time_exec(num_runs):
     import time
+    from tqdm import tqdm
 
-    # t0 = time.time()
-    # for j in range(1000000):
-    #     pass
-    # t1 = time.time()
-    # print("Выполнение заняло %.5f секунд" % (t1 - t0))
-
-    import time
-
-    NUM_RUNS = 10
+    NUM_RUNS = num_runs
 
     avg_time = 0
-    for _ in range(NUM_RUNS):
+    for _ in tqdm(range(NUM_RUNS)):
         t0 = time.time()
 
         ### <<полезный>> код
@@ -21,8 +41,14 @@ def main():
 
         t1 = time.time()
         avg_time += (t1 - t0)
+        # print(t1 - t0)
     avg_time /= NUM_RUNS
-    print("Выполнение заняло %.5f секунд" % avg_time)
+    print("Выполнение задания в среднем занимает %.5f секунд" % avg_time)
+
+
+def main():
+
+    avg_time_exec(10)
 
 
 if __name__ == "__main__":
